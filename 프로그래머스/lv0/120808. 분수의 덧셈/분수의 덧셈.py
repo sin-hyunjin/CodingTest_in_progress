91466a3eb49ae2aa484afa2679908dda9b56da9d
sin-hyunjin <116487398+sin-hyunjin@users.noreply.github.com>
2023-07-12 17:04:07 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.56 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,11 +1,14 @@
 def solution(numer1, denom1, numer2, denom2):
     boonmo = 0
+#     최소공배수
     for i in range(max(denom1, denom2), (denom1 * denom2) + 1): 
         if i % denom1 == 0 and i % denom2 == 0:
             boonmo = int(i)
             break
+            
     boonja = int((boonmo / denom1) * numer1 + (boonmo / denom2) * numer2)
     
+#     최대공약수
     for n in range(min(boonja,boonmo),0,-1): 
         if boonmo%n == 0 and boonja%n == 0:
             boonmo /= n
