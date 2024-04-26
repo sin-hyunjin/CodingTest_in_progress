47bf70c31b69e010ba3a69e9cbbeda07dacda65f
현진이다 <116487398+sin-hyunjin@users.noreply.github.com>
2024-04-26 23:36:57 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,12 @@
+def solution(price):
+    discount = 0
+    
+    if price >= 500000:
+        discount = 0.2
+    elif price >= 300000:
+        discount = 0.1
+    elif price >= 100000:
+        discount = 0.05
+    
+    discounted_price = price * (1 - discount)
+    return int(discounted_price)
